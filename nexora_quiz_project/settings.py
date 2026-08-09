b612edc398b6d9bcd054df3d2d33d36119c6bd0b
boddu-poojitha <boddupoojitha1@gmail.com>
2026-08-09 09:05:57 +0530

    Secure configuration and clean Git tracking

--- nexora_quiz_project/settings.py
+++ nexora_quiz_project/settings.py
@@ -12,16 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--ek-37yli%dscwounml$m(-7jai42*=-m5o8e4lzz-89#ib@va'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'nexora_quiz_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'quiz_db1', # The database name you created in MySQL Workbench
-        'USER': 'admin',      # Your MySQL username
-        'PASSWORD': 'chaithanyakumar', # !! IMPORTANT: Replace with your actual MySQL root/user password
-        'HOST': 'my-first-db.chdm9fkbg1ke.us-east-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
+        }
     }
-}
 }
 
 # Password validation
@@ -129,10 +129,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nexoraquiz@gmail.com'  # Your Gmail address
-EMAIL_HOST_PASSWORD = 'mpus sryr ohtt nobs'  # Generate an app password for Gmail
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
